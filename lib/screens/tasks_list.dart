c41909379992e4e4b71cbe65e19ce1138e7d5886
MeriemAfafHaddou <jm_haddou@esi.dz>
2023-03-27 00:54:00 +0100

    search and sort tasks

--- lib/screens/tasks_list.dart
+++ lib/screens/tasks_list.dart
@@ -13,30 +13,95 @@ class Task{
   Task(this.id, this.date, this.heure, this.entreprise, this.message, this.type, this.etat);
 }
 List<Task> taskslist=[
-  Task(1,'04 mars 2023','03:00 PM','ESI Alger', "Manque d'ingredients",'Rupture de stock', "TO DO"),
-  Task(2,'05 mars 2023','07:00 PM','EPAU', "Manque d'ingredients",'Rupture de stock', "TO DO"),
+  Task(1,'04 mars 2023','03:00 PM','ESI Alger', "Mecanisme bloque",'Rupture de stock', "TO DO"),
+  Task(2,'05 mars 2023','07:00 PM','EPAU', "Disconnected",'Rupture de stock', "TO DO"),
   Task(3,'05 mars 2023','09:00 AM','USTHB', "Manque d'ingredients",'Rupture de stock', "DONE"),
   Task(4,'06 mars 2023','04:00 PM','SOGRAL', "Manque d'ingredients",'Rupture de stock', "TO DO"),
   Task(5,'06 mars 2023','08:00 AM','City Center Alger', "Manque d'ingredients",'Rupture de stock', "DONE"),
   Task(6,'06 mars 2023','04:50 PM','CUB1', "Manque d'ingredients",'Rupture de stock', "TO DO"),
 ];
-class TasksList extends StatelessWidget {
-  const TasksList({Key? key}) : super(key: key);
+class TasksList extends StatefulWidget{
+  const TasksList({super.key});
   @override
-  Widget build(BuildContext context) {
+  State<StatefulWidget> createState() => TasksListState();
+}
+class TasksListState extends State<TasksList> {
+  TasksListState({Key? key}) : super();
+  late List<Task> displayedList=List.from(taskslist);
+  late String sortValue='Date';
+  void searchTasks(String search) {
+    setState(() {
+      displayedList = taskslist
+          .where((item) =>
+          item.message.toLowerCase().contains(search.toLowerCase()))
+          .toList();
+    });
+  }
+  void sortTasks(String? value){
+    setState(() {
+      sortValue = value!;
+      displayedList.sort((a, b) {
+        if (value == 'Date') {
+          return a.date.compareTo(b.date);
+        } else if (value == 'Identifier') {
+          return a.id.compareTo(b.id);
+        } else {
+          return a.etat.compareTo(b.etat);
+        }
+      });
+    });
+  }
+  @override
+    Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+              color: cadetGray,
+            ),
+          ),
+          Expanded(
+            child: TextField(
+              style: const TextStyle(
+                color: cadetGray,
+              ),
+              decoration: const InputDecoration(
+                hintText: 'Search',
+                border: InputBorder.none,
+              ),
+              onChanged: (value) => searchTasks(value),
+            ),
+          ),
+          DropdownButton<String>(
+            value: sortValue,
+            onChanged: sortTasks,
+            items: <String>['Date', 'Identifier', 'State']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
       body: ListView.builder(
         padding: const EdgeInsets.all(8),
-        itemCount: taskslist.length,
+        itemCount: displayedList.length,
         itemBuilder: (context, index){
-          final task = taskslist[index];
+          final task = displayedList[index];
           return ListTile(
             title: GestureDetector(
               onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => TaskDetails(key: key,task: task),
+                    builder: (context) => TaskDetails(task: task),
                   ),
                 );
               },
@@ -52,7 +117,6 @@ class TasksList extends StatelessWidget {
                 child:Column(
                   children: [
                     Container(
-
                       padding:const EdgeInsets.only(bottom:20),
                       child:  Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -75,33 +139,35 @@ class TasksList extends StatelessWidget {
                       ),
                     ),
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        const Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Distributeur :',
-                              style: TextStyle(
-                                fontSize: 13,
-                                color: Colors.black,
+                        Container(
+                          margin: const EdgeInsets.only(right:15),
+                          child: const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Distributeur :',
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.black,
+                                ),
                               ),
-                            ),
-                            Text(
-                              'Message :',
-                              style: TextStyle(
-                                fontSize: 13,
-                                color: Colors.black,
+                              Text(
+                                'Message :',
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.black,
+                                ),
                               ),
-                            ),
-                            Text(
-                              'Etat :',
-                              style: TextStyle(
-                                fontSize: 13,
-                                color: Colors.black,
+                              Text(
+                                'Etat :',
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.black,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
